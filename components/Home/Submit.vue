41e37e2fa3b699bc164b5621ebf136710867fdf7
Peter Csipkay <hello@petercsipkay.com>
2021-09-10 12:01:47 +0200

    dss

--- components/Home/Submit.vue
+++ components/Home/Submit.vue
@@ -43,72 +43,70 @@
 					<label class="mt-3 block text-lg text-gray-700">
 						Select Channel
 					</label>
-					<div class="">
-						<fieldset>
-							<div class="space-y-4">
-								<div class="md:flex  space-x-5">
-									<label @click="selectChannel(1)" class="md:w-1/2 relative block rounded-lg border border-gray-300 bg-white text-black px-6 py-3 cursor-pointer sm:flex sm:justify-between opacity-50 transition duration-500" :class="{ 'opacity-100 bg-red': activeChannel == 1 }">
-										<input type="radio" name="socialchannels" v-model="socialChannel" value="welikeuidesign" class="sr-only" aria-labelledby="welikeuidesign" aria-describedby="welikeuidesign" />
-										<div class="flex items-center">
-											<div class="text-sm">
-												<p id="welikeuidesign" class="text-lg transition duration-500" :class="{ 'text-white': activeChannel == 1 }">
-													@welikeuidesign
-												</p>
-											</div>
-										</div>
-									</label>
 
-									<label @click="selectChannel(2)" class="md:w-1/2 relative block rounded-lg border border-gray-300 bg-white px-6 py-3 cursor-pointer sm:flex sm:justify-between opacity-50 transition duration-500" :class="{ 'opacity-100 bg-blue': activeChannel == 2 }">
-										<input type="radio" name="socialchannels" v-model="socialChannel" value="welikewebgl" class="sr-only" aria-labelledby="welikewebgl" aria-describedby="welikewebgl" />
-										<div class="flex items-center">
-											<div class="text-sm">
-												<p id="welikewebgl" class="text-lg transition duration-500" :class="{ 'text-white': activeChannel == 2 }">
-													@welikewebgl
-												</p>
-											</div>
-										</div>
-									</label>
+					<div class="space-y-4">
+						<div class="md:flex  space-x-5">
+							<label @click="selectChannel(1)" class="md:w-1/2 relative block rounded-lg border border-gray-300 bg-white text-black px-6 py-3 cursor-pointer sm:flex sm:justify-between opacity-50 transition duration-500" :class="{ 'opacity-100 bg-red': activeChannel == 1 }">
+								<input type="radio" name="socialchannels" v-model="socialChannel" value="welikeuidesign" class="sr-only" aria-labelledby="welikeuidesign" aria-describedby="welikeuidesign" />
+								<div class="flex items-center">
+									<div class="text-sm">
+										<p id="welikeuidesign" class="text-lg transition duration-500" :class="{ 'text-white': activeChannel == 1 }">
+											@welikeuidesign
+										</p>
+									</div>
 								</div>
-								<div class="md:flex space-x-5">
-									<label @click="selectChannel(3)" class="md:w-1/2  relative block rounded-lg border border-gray-300 bg-white px-6 py-3 cursor-pointer sm:flex sm:justify-between opacity-50 transition duration-500" :class="{ 'opacity-100 bg-neutral-dark': activeChannel == 3 }">
-										<input type="radio" name="socialchannels" v-model="socialChannel" value="welikemotiondesign" class="sr-only" aria-labelledby="welikemotiondesign" aria-describedby="welikemotiondesign" />
-										<div class="flex items-center">
-											<div class="text-sm">
-												<p id="welikemotiondesign" class="text-lg transition duration-500" :class="{ 'text-white': activeChannel == 3 }">
-													@welikemotiondesign
-												</p>
-											</div>
-										</div>
-									</label>
+							</label>
 
-									<label @click="selectChannel(4)" class="md:w-1/2 relative block rounded-lg border border-gray-300 bg-white px-6 py-3 cursor-pointer sm:flex sm:justify-between opacity-50 transition duration-500" :class="{ 'opacity-100 bg-purple': activeChannel == 4 }">
-										<input type="radio" name="socialchannels" v-model="socialChannel" value="welikethreedy" class="sr-only" aria-labelledby="welikethreedy" aria-describedby="welikethreedy" />
-										<div class="flex items-center">
-											<div class="text-sm">
-												<p id="welikethreedy" class="text-lg transition duration-500" :class="{ 'text-white': activeChannel == 4 }">
-													@welikethreedy
-												</p>
-											</div>
-										</div>
-									</label>
+							<label @click="selectChannel(2)" class="md:w-1/2 relative block rounded-lg border border-gray-300 bg-white px-6 py-3 cursor-pointer sm:flex sm:justify-between opacity-50 transition duration-500" :class="{ 'opacity-100 bg-blue': activeChannel == 2 }">
+								<input type="radio" name="socialchannels" v-model="socialChannel" value="welikewebgl" class="sr-only" aria-labelledby="welikewebgl" aria-describedby="welikewebgl" />
+								<div class="flex items-center">
+									<div class="text-sm">
+										<p id="welikewebgl" class="text-lg transition duration-500" :class="{ 'text-white': activeChannel == 2 }">
+											@welikewebgl
+										</p>
+									</div>
 								</div>
-								<div class="md:flex space-x-5">
-									<label @click="selectChannel(5)" class="md:w-1/2  relative block rounded-lg border border-gray-300 bg-white px-6 py-3 cursor-pointer sm:flex sm:justify-between opacity-50 transition duration-500" :class="{ 'opacity-100 bg-orange': activeChannel == 5 }">
-										<input type="radio" name="socialchannels" v-model="socialChannel" value="welikeillustration" class="sr-only" aria-labelledby="welikeillustration" aria-describedby="welikeillustration" />
-										<div class="flex items-center">
-											<div class="text-sm">
-												<p id="welikeillustration" class="text-lg transition duration-500" :class="{ 'text-white': activeChannel == 5 }">
-													@welikeillustration
-												</p>
-											</div>
-										</div>
-									</label>
-									<div class="md:w-1/2"></div>
+							</label>
+						</div>
+						<div class="md:flex space-x-5">
+							<label @click="selectChannel(3)" class="md:w-1/2  relative block rounded-lg border border-gray-300 bg-white px-6 py-3 cursor-pointer sm:flex sm:justify-between opacity-50 transition duration-500" :class="{ 'opacity-100 bg-neutral-dark': activeChannel == 3 }">
+								<input type="radio" name="socialchannels" v-model="socialChannel" value="welikemotiondesign" class="sr-only" aria-labelledby="welikemotiondesign" aria-describedby="welikemotiondesign" />
+								<div class="flex items-center">
+									<div class="text-sm">
+										<p id="welikemotiondesign" class="text-lg transition duration-500" :class="{ 'text-white': activeChannel == 3 }">
+											@welikemotiondesign
+										</p>
+									</div>
 								</div>
-							</div>
-						</fieldset>
+							</label>
+
+							<label @click="selectChannel(4)" class="md:w-1/2 relative block rounded-lg border border-gray-300 bg-white px-6 py-3 cursor-pointer sm:flex sm:justify-between opacity-50 transition duration-500" :class="{ 'opacity-100 bg-purple': activeChannel == 4 }">
+								<input type="radio" name="socialchannels" v-model="socialChannel" value="welikethreedy" class="sr-only" aria-labelledby="welikethreedy" aria-describedby="welikethreedy" />
+								<div class="flex items-center">
+									<div class="text-sm">
+										<p id="welikethreedy" class="text-lg transition duration-500" :class="{ 'text-white': activeChannel == 4 }">
+											@welikethreedy
+										</p>
+									</div>
+								</div>
+							</label>
+						</div>
+						<div class="md:flex space-x-5">
+							<label @click="selectChannel(5)" class="md:w-1/2  relative block rounded-lg border border-gray-300 bg-white px-6 py-3 cursor-pointer sm:flex sm:justify-between opacity-50 transition duration-500" :class="{ 'opacity-100 bg-orange': activeChannel == 5 }">
+								<input type="radio" name="socialchannels" v-model="socialChannel" value="welikeillustration" class="sr-only" aria-labelledby="welikeillustration" aria-describedby="welikeillustration" />
+								<div class="flex items-center">
+									<div class="text-sm">
+										<p id="welikeillustration" class="text-lg transition duration-500" :class="{ 'text-white': activeChannel == 5 }">
+											@welikeillustration
+										</p>
+									</div>
+								</div>
+							</label>
+							<div class="md:w-1/2"></div>
+						</div>
 					</div>
 				</div>
+
 				<div class="btn mt-5">
 					<button type="submit" class="text-center cursor-pointer">Submit</button>
 				</div>
